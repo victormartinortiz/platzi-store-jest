f0568e1ebb78a299c24d9c3762e1fde5e06b32dd
Martin Ortiz <martinortizpalacio@gmail.com>
2020-01-09 12:12:02 -0300

    added test of snapshot to header, and footer

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
@@ -11,5 +12,12 @@ describe('<Footer />', () => {
 
   test('Render title', () => {
     expect(footer.find('.Footer-title').text()).toMatch('Platzi Store');
+  });
+});
+
+describe('Footer Snapshot', () => {
+  test('Verify the UI component', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
   });
 });
